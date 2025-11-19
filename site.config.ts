37cc3e8b452323fd8e368bc6c71606fa2120214a
kimsh1017 <kimsh8580@gmail.com>
2025-11-19 20:57:49 +0900

    pageId 변경

--- site.config.ts
+++ site.config.ts
@@ -2,14 +2,14 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '2b0e945eaed08106bdd1f860c8df8cd1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '김성훈의 블로그',
+  name: '김성훈의 개발 블로그',
   domain: 'https://kimsh1017-dev-blog.vercel.app/',
   author: '김성훈',
 
