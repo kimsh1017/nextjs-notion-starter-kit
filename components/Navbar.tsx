8e97bfad9d92aedc829263b76dc958d71fcb4ff4
kimsh1017 <kimsh8580@gmail.com>
2025-11-22 09:54:22 +0900

    Navbar에 다크모드 색 추가

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,36 @@
 // components/Navbar.tsx
+import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
+import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+import cs from 'classnames'
 import Link from 'next/link'
+import * as React from 'react'
 import siteConfig from 'site.config'
 
+import { useDarkMode } from '@/lib/use-dark-mode'
+
 import styles from './Navbar.module.css'
+
+function ToggleThemeButton() {
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const { isDarkMode, toggleDarkMode } = useDarkMode()
+
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  const onToggleTheme = React.useCallback(() => {
+    toggleDarkMode()
+  }, [toggleDarkMode])
+
+  return (
+    <div
+      className={cs('breadcrumb', 'button', !hasMounted && styles.hidden)}
+      onClick={onToggleTheme}
+    >
+      {hasMounted && isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+    </div>
+  )
+}
 
 export function Navbar() {
   return (
@@ -13,7 +41,7 @@ export function Navbar() {
         </Link>
       </div>
       <div className={styles.navbarRight}>
-        {/* Future navigation items can go here */}
+        <ToggleThemeButton />
       </div>
     </nav>
   )
