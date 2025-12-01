86bc35e19e929b5c399761bab43bea3714372437
kimsh1017 <kimsh8580@gmail.com>
2025-12-01 21:12:39 +0900

    첫화면 Memoize 활용

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,3 +1,4 @@
+import ExpiryMap from 'expiry-map'
 import {
   type ExtendedRecordMap,
   type SearchParams,
@@ -41,31 +42,36 @@ const getNavigationLinkPages = pMemoize(
   }
 )
 
-export async function getPage(pageId: string): Promise<ExtendedRecordMap> {
-  let recordMap = await notion.getPage(pageId)
+export const getPage = pMemoize(
+  async (pageId: string): Promise<ExtendedRecordMap> => {
+    let recordMap = await notion.getPage(pageId)
 
-  if (navigationStyle !== 'default') {
-    // ensure that any pages linked to in the custom navigation header have
-    // their block info fully resolved in the page record map so we know
-    // the page title, slug, etc.
-    const navigationLinkRecordMaps = await getNavigationLinkPages()
+    if (navigationStyle !== 'default') {
+      // ensure that any pages linked to in the custom navigation header have
+      // their block info fully resolved in the page record map so we know
+      // the page title, slug, etc.
+      const navigationLinkRecordMaps = await getNavigationLinkPages()
 
-    if (navigationLinkRecordMaps?.length) {
-      recordMap = navigationLinkRecordMaps.reduce(
-        (map, navigationLinkRecordMap) =>
-          mergeRecordMaps(map, navigationLinkRecordMap),
-        recordMap
-      )
+      if (navigationLinkRecordMaps?.length) {
+        recordMap = navigationLinkRecordMaps.reduce(
+          (map, navigationLinkRecordMap) =>
+            mergeRecordMaps(map, navigationLinkRecordMap),
+          recordMap
+        )
+      }
     }
-  }
 
-  if (isPreviewImageSupportEnabled) {
-    const previewImageMap = await getPreviewImageMap(recordMap)
-    ;(recordMap as any).preview_images = previewImageMap
-  }
+    if (isPreviewImageSupportEnabled) {
+      const previewImageMap = await getPreviewImageMap(recordMap)
+      ;(recordMap as any).preview_images = previewImageMap
+    }
 
-  return recordMap
-}
+    return recordMap
+  },
+  {
+    cache: new ExpiryMap(10_000)
+  }
+)
 
 export async function search(params: SearchParams): Promise<SearchResults> {
   return notion.search(params)
